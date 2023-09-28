4cb94a6bd42cf2f253222dc196506a1e4a398145
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-09-28 11:53:39 -0600

    "Integrate Spotify SDK in SearchResults.js"
    Updated the SearchResults.js file to use the Spotify SDK. Imported the Spotify SDK and refactored the addTopFive, addTopTen, and addAll methods to use the SDK's addTracksToPlaylist method. The track URIs are now extracted from the search results and passed to the Spotify SDK.
    
    The motivation for this change was to streamline the process of adding tracks to the playlist by utilizing the Spotify SDK's built-in methods. This differs from the previous implementation where we were using fetch-based API calls. Now, the Spotify SDK handles these operations, making the code cleaner and more efficient.

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,35 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import './SearchResults.css';
+import "./SearchResults.css";
 
-import TrackList from '../TrackList/TrackList.js';
+import TrackList from "../TrackList/TrackList.js";
+
+import Spotify from "../../util/Spotify";
 
 class SearchResults extends React.Component {
-    constructor(props) {
-        super(props);
-        this.addTopFive = this.addTopFive.bind(this);
-        this.addTopTen = this.addTopTen.bind(this);
-        this.addAll = this.addAll.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.addTopFive = this.addTopFive.bind(this);
+    this.addTopTen = this.addTopTen.bind(this);
+    this.addAll = this.addAll.bind(this);
+  }
 
-    //add the top five tracks to the playlist
-    addTopFive() {
-        this.props.onAdd(this.props.searchResults.slice(0, 5));
-    }
+  //add the top five tracks to the playlist
+  addTopFive() {
+    Spotify.addTracksToPlaylist(
+      this.props.playlistId,
+      this.props.searchResults.slice(0, 5).map((track) => track.uri),
+    );
+  }
 
-    //add top 10 tracks to the playlist
-    addTopTen() {
-        this.props.onAdd(this.props.searchResults.slice(0, 10));
-    }
+  //add top 10 tracks to the playlist
+  addTopTen() {
+    Spotify.addTracksToPlaylist(
+      this.props.playlistId,
+      this.props.searchResults.slice(0, 10).map((track) => track.uri),
+    );
+  }
 
-    addAll() {
-        this.props.onAdd(this.props.searchResults);
-    }
-    render() {
+  addAll() {
+    Spotify.addTracksToPlaylist(
+      this.props.playlistId,
+      this.props.searchResults.map((track) => track.uri),
+    );
+  }
+  render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} onToggle={this.props.onToggle}  currentTrack={this.props.currentTrack}/>
+        <TrackList
+          tracks={this.props.searchResults}
+          onAdd={this.props.onAdd}
+          onToggle={this.props.onToggle}
+          currentTrack={this.props.currentTrack}
+        />
       </div>
     );
   }
