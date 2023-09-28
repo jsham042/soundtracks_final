5c2ddad97a0632a283a50692e3288e4998b4ff7d
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-09-28 11:52:46 -0600

    "Update LoginPage to use Spotify SDK for login"
    In this commit, the LoginPage component has been updated to use the Spotify SDK for handling user login. Previously, the onLogin prop was used to handle the login process. Now, a local handleLogin function has been created that calls the Spotify.getAccessToken() method when the login button is clicked. 
    
    This change was motivated by the need to streamline the login process and make use of the Spotify SDK's built-in functionality. The previous implementation relied on a prop passed down from a parent component, which added unnecessary complexity to the component hierarchy. By using the Spotify SDK directly in the LoginPage component, we can simplify the code and improve maintainability.

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import './LoginPage.css';
+import React from "react";
+import "./LoginPage.css";
+import Spotify from "../../util/Spotify";
 
-const LoginPage = ({ onLogin }) => {
-    return (
-        <div className="LoginPage">
-            <div className="App-logo">
-                <img src="/djboticon.png" alt="App Logo" />
-            </div>
-            <h1>SOUND<span className="highlight">TRACKS</span></h1>
-            <p className="subtitle">Get music to perfectly match the vibe you're going for</p>
-            <button className="login-button" onClick={onLogin}>
-                Connect Your Spotify Account
-            </button>
-            <p className="disclaimer">
-                *You need a Spotify Premium account to use this app
-            </p>
-        </div>
-    );
+const LoginPage = () => {
+  const handleLogin = async () => {
+    await Spotify.getAccessToken();
+  };
+
+  return (
+    <div className="LoginPage">
+      <div className="App-logo">
+        <img src="/djboticon.png" alt="App Logo" />
+      </div>
+      <h1>
+        SOUND<span className="highlight">TRACKS</span>
+      </h1>
+      <p className="subtitle">
+        Get music to perfectly match the vibe you're going for
+      </p>
+      <button className="login-button" onClick={handleLogin}>
+        Connect Your Spotify Account
+      </button>
+      <p className="disclaimer">
+        *You need a Spotify Premium account to use this app
+      </p>
+    </div>
+  );
 };
-
 export default LoginPage;
