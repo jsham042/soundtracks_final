9518e7c9454fd179572dcfdcdc7b96c6a2d03906
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-09-28 19:06:12 -0600

    "Update getAccessToken function in Spotify.js"
    Updated the getAccessToken function in src/util/Spotify.js to use the Spotify SDK's clientCredentialsGrant method for authentication. This change was motivated by the need to improve the efficiency and security of our authentication process. 
    
    Previously, the getAccessToken function was using a less secure and less efficient method for authentication. The new implementation retrieves an access token from Spotify's API using the client ID and secret, which is more secure and efficient. The access token is then set for the Spotify API instance and stored in the accessToken variable. This change ensures that our application is using the most up-to-date and secure methods for interacting with Spotify's API.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,31 +1,31 @@
 import SpotifyWebApi from "spotify-web-api-js";
 
-const clientId = process.env.REACT_APP_MY_SPOTIFY_CLIENT_ID; // client ID  that Joe got from registering the app
+const clientId = process.env.REACT_APP_MY_SPOTIFY_CLIENT_ID;
 
-const redirectUri = "https://www.soundtracksai.com/"; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
+const redirectUri = "https://www.soundtracksai.com/";
 let accessToken;
 
 const spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken(accessToken);
 
 const Spotify = {
-  getAccessToken: function () {
+  getAccessToken: async function () {
     if (accessToken) {
       return accessToken;
     }
 
-    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-    if (accessTokenMatch && expiresInMatch) {
-      accessToken = accessTokenMatch[1];
-      const expiresIn = Number(expiresInMatch[1]);
-      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
-      window.history.pushState("Access Token", null, "/"); // This clears the parameters, allowing us to grab a new access token when it expires.
-      return accessToken;
-    } else {
-      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
-      window.location = accessUrl;
+    try {
+      const data = await spotifyApi.clientCredentialsGrant();
+      console.log("The access token expires in " + data.body["expires_in"]);
+      console.log("The access token is " + data.body["access_token"]);
+
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body["access_token"]);
+      accessToken = data.body["access_token"];
+    } catch (err) {
+      console.log("Something went wrong when retrieving an access token", err);
     }
+
+    return accessToken;
   },
   getUserInfo: function () {
     return spotifyApi.getMe().then((jsonResponse) => {
