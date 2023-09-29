c7fc669246269593bc34350ccbc811e9aef3ca3c
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-09-28 19:08:00 -0600

    "Add 'searchTracks' function in src/util/Spotify.js"
    This commit introduces a new function 'searchTracks' in the Spotify.js utility file. The function leverages the Spotify SDK's track search method to search for tracks based on a provided search term. The response from the Spotify SDK is then mapped to an array of track objects.
    
    The motivation for this change was to enhance the track search functionality in our application. The new implementation differs from the previous one as it now uses the Spotify SDK's track search method, which provides a more efficient and reliable way to search for tracks. This change is in line with the tasks [<modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B676DE20>, <modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B676D670>, <modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B678F5B0>, <modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B678F820>, <modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B678F3D0>, <modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B678F6D0>, <modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B678FB20>, <modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B678F2E0>, <modules.build_module_backend.services.TaskCreation.Task.Task object at 0x000002B8B678F460>].

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -38,6 +38,22 @@ const Spotify = {
       };
     });
   },
+  searchTracks: function (term) {
+    return spotifyApi.searchTracks(term).then((jsonResponse) => {
+      if (!jsonResponse) {
+        return [];
+      }
+      return jsonResponse.tracks.items.map((track) => {
+        return {
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri,
+        };
+      });
+    });
+  },
   // Rest of the code remains the same
 };
 export default Spotify;
