8d55e5752cd2a90323d6f961afdffba2252ce33b
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-11-07 11:02:58 -0800

    commit_message="Control SearchBar input with state\n\nThe SearchBar component's input field is now controlled by the component's state to ensure consistency and better manage the user input.\n\nThe motivation for this change was to enhance the reliability of the SearchBar component by ensuring that the input's value is always in sync with the component's state. This change was inspired by tasks that required a more predictable and controlled input behavior within the application.\n\nCompared to the previous implementation, the updated SearchBar now:\n\n- Explicitly sets the input value from the component's state.\n- Ensures that any change to the input updates the state accordingly, maintaining a single source of truth.\n\nThis update is a key step towards a more robust and maintainable codebase, particularly in the way user input is handled across the application."

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './SearchBar.css';
-import { faSearch} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./SearchBar.css";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class SearchBar extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      term: localStorage.getItem('searchTerm') || ''
+      term: localStorage.getItem("searchTerm") || "",
     };
 
     this.handleTermChange = this.handleTermChange.bind(this);
@@ -18,43 +18,35 @@ constructor(props) {
   }
 
   handleTermChange(event) {
-    this.setState({term: event.target.value});
+    this.setState({ term: event.target.value });
   }
 
   search() {
-    localStorage.setItem('searchTerm', this.state.term);
+    localStorage.setItem("searchTerm", this.state.term);
     this.props.onSearch(this.state.term);
   }
-    if (event.key === 'Enter') {
-      this.search();
-    }
-  }
-
   render() {
     return (
       <div className="SearchBar">
         <input
-            placeholder="Describe the vibe you're going for"
-            onChange={this.handleTermChange}
-            onKeyDown={this.handleKeyDown}
-            />
+          placeholder="Describe the vibe you're going for"
+          value={this.state.term}
+          onChange={this.handleTermChange}
+          onKeyDown={this.handleKeyDown}
+        />
         <button onClick={this.search}>
           <FontAwesomeIcon icon={faSearch} />
-
         </button>
       </div>
     );
   }
-}
-
-export default SearchBar;
-search() {
-    localStorage.setItem('searchTerm', this.state.term);
+  search() {
+    localStorage.setItem("searchTerm", this.state.term);
     this.props.onSearch(this.state.term);
   }
 
   handleKeyDown(event) {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       this.search();
     }
   }
@@ -63,13 +55,12 @@ search() {
     return (
       <div className="SearchBar">
         <input
-            placeholder="Describe the vibe you're going for"
-            onChange={this.handleTermChange}
-            onKeyDown={this.handleKeyDown}
-            />
+          placeholder="Describe the vibe you're going for"
+          onChange={this.handleTermChange}
+          onKeyDown={this.handleKeyDown}
+        />
         <button onClick={this.search}>
           <FontAwesomeIcon icon={faSearch} />
-
         </button>
       </div>
     );
